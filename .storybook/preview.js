a1615b65dc46f0b9adcf75be59368e2be992b751
Yann Gouffon <github@yago-art.ch>
2020-12-17 09:38:07 +0100

    ✨ add HTML code tab

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  html: {
+    prettier: {
+      tabWidth: 2,
+      useTabs: false,
+    },
+  },
+  docs: {
+    source: {
+      code: 'Take a look in the HTML tab of your Canvas view',
+    },
+  },
 }
