faf6671f2c8f8bd73cde2618b9917dc8b0fa2711
Yann Gouffon <github@yago-art.ch>
2021-01-05 08:43:42 +0100

    📝 add better module example

--- src/modules/header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Icon from 'components/atoms/Icon';
-
-const root = document.getElementById('uww-grid');
-
-ReactDOM.render(<Icon {...root?.dataset} />, root);
